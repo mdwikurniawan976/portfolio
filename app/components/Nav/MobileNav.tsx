f821b6a4325537dea6097d5d9114cf7a7da06781
M Dwi Kurniawan <124229813+mdwikurniawan976@users.noreply.github.com>
2025-06-13 04:27:33 +0700

    Update MobileNav.tsx

--- app/components/Nav/MobileNav.tsx
+++ app/components/Nav/MobileNav.tsx
@@ -16,17 +16,17 @@ const MobileNav = ({ showNav, closeNav }: Props) => {
         <div>
             {/* overlay */}
             <div
-                className={`fixed ${navOpen} inset-0 transform transition-all duration-500 z-[1002] bg-[#8B5CF6] opacity-70 w-full h-screen`}
+                className={`fixed ${navOpen} inset-0 transform transition-all duration-500 z-[1002] bg-[#0F0F1A] opacity-70 w-full h-screen`}
             ></div>
+
             {/* navlinks */}
             <div
-                className={`${navOpen} text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-[#8B5CF6]  space-y-6 z-[1050]`}
+                className={`${navOpen} text-white fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-[#1A1A2F] space-y-6 z-[1050]`}
             >
                 {navLinks.map((link) => {
                     return (
                         <Link key={link.id} href={link.url}>
                             <p className="text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]">
-                                {" "}
                                 {link.label}
                             </p>
                         </Link>
@@ -37,6 +37,7 @@ const MobileNav = ({ showNav, closeNav }: Props) => {
                     className="absolute top-[0.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6"
                 />
             </div>
+
         </div>
     );
 };
